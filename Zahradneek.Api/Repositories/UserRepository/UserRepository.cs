300816ae7633b2b94567ea4329ed04becaface79
Filip S <filip.syn@protonmail.com>
2023-04-06 22:18:58 +0200

    Created user repository implementation

---
+++ Zahradneek.Api/Repositories/UserRepository/UserRepository.cs
@@ -0,0 +1,46 @@
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Zahradneek.Api.Data;
+using Zahradneek.Api.Models;
+
+namespace Zahradneek.Api.Repositories.UserRepository;
+
+public class UserRepository : IUserRepository
+{
+    private readonly DataContext _db;
+
+    public UserRepository(DataContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<User?> GetByIdAsync(Guid userId)
+    {
+        return await _db.Users.FirstOrDefaultAsync(user => user.Id == userId);
+    }
+
+    public async Task<IEnumerable<User>> GetAllAsync()
+    {
+        return await _db.Users.ToListAsync();
+    }
+
+    public async Task<IEnumerable<User>> GetWhereAsync(Expression<Func<User, bool>> predicate)
+    {
+        return await _db.Users.Where(predicate: predicate).ToListAsync();
+    }
+
+    public Task<bool> CreateAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteByIdAsync(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+}
