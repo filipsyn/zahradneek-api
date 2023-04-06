720a48d3b3643094d4a4866cff00d4e815844f93
Filip S <filip.syn@protonmail.com>
2023-04-06 20:22:35 +0200

    Created interface for user repository

---
+++ Zahradneek.Api/Repositories/UserRepository/IUserRepository.cs
@@ -0,0 +1,18 @@
+using Zahradneek.Api.Models;
+
+namespace Zahradneek.Api.Repositories.UserRepository;
+
+public interface IUserRepository
+{
+   public Task<User?> GetByIdAsync(Guid userId);
+
+   public Task<IEnumerable<User>> GetAllAsync();
+
+   public Task<IEnumerable<User>> GetWhereAsync(Func<User, bool> predicate);
+
+   public Task<bool> CreateAsync(User user);
+
+   public Task<bool> UpdateAsync(User user);
+
+   public Task<bool> DeleteByIdAsync(Guid userId);
+}
