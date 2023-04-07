a3eff88677c1a063a930a790c561a99ab5c9c6d9
Filip S <filip.syn@protonmail.com>
2023-04-07 15:41:18 +0200

    Added global exception handling middleware
    This middleware catches all thrown exceptions and turns them into error response

---
+++ Zahradneek.Api/Middleware/GlobalExceptionHandlingMiddleware.cs
@@ -0,0 +1,54 @@
+using System.Text.Json;
+using Zahradneek.Api.Exceptions;
+
+namespace Zahradneek.Api.Middleware;
+
+public class GlobalExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public GlobalExceptionHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context: context, ex: ex);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception ex)
+    {
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = GetStatusCode(ex);
+        
+        var exceptionResult = JsonSerializer.Serialize(new
+        {
+            message = ex.Message,
+            //StackTrace = ex.StackTrace,
+            //statusCode = context.Response.StatusCode
+        });
+
+        return context.Response.WriteAsync(exceptionResult);
+    }
+
+    private static int GetStatusCode(Exception ex)
+    {
+        return ex.GetType() switch
+        {
+            { } t when t == typeof(NotFoundException) => StatusCodes.Status404NotFound,
+            { } t when t == typeof(ValidationException) => StatusCodes.Status400BadRequest,
+            { } t when t == typeof(DbConflictException) => StatusCodes.Status409Conflict,
+            { } t when t == typeof(ConflictException) => StatusCodes.Status409Conflict,
+            _ => StatusCodes.Status500InternalServerError
+        };
+    }
+}
