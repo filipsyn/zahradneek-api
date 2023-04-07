b7de9d6d34c23910375c300f9f2bc78262ac90ae
Filip S <filip.syn@protonmail.com>
2023-04-07 11:49:07 +0200

    Added injection of mapper and repository to user service

--- Zahradneek.Api/Services/UserService/UserService.cs
+++ Zahradneek.Api/Services/UserService/UserService.cs
@@ -1,9 +1,20 @@
+using AutoMapper;
 using Zahradneek.Api.Contracts.v1;
+using Zahradneek.Api.Repositories.UserRepository;
 
 namespace Zahradneek.Api.Services.UserService;
 
 public class UserService : IUserService
 {
+    private readonly IMapper _mapper;
+    private readonly IUserRepository _userRepository;
+
+    public UserService(IMapper mapper, IUserRepository userRepository)
+    {
+        _mapper = mapper;
+        _userRepository = userRepository;
+    }
+
     public async Task<UserInfoResponse> GetByIdAsync(Guid userId)
     {
         throw new NotImplementedException();
