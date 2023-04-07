b45ad9f546b71d83f8a7791972cdae2ae9fb97a1
Filip S <filip.syn@protonmail.com>
2023-04-07 15:25:46 +0200

    Implemented getting of users to controller

--- Zahradneek.Api/Controllers/v1/UsersController.cs
+++ Zahradneek.Api/Controllers/v1/UsersController.cs
@@ -1,15 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
+using Zahradneek.Api.Services.UserService;
 
 namespace Zahradneek.Api.Controllers.v1;
 
 [ApiController]
 [Route("/users")]
-public class UsersController: ControllerBase
+public class UsersController : ControllerBase
 {
+    private readonly IUserService _userService;
+
+    public UsersController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
     [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok(await _userService.GetAllAsync());
+    }
+
+    [HttpGet("{userId}")]
     public async Task<IActionResult> GetById(Guid userId)
     {
-        return Ok();
-            
+        return Ok(await _userService.GetByIdAsync(userId));
     }
 }
