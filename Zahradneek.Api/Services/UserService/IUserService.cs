534e3518ed0d2043c6df157d1cb9ee4ca35c3d6a
Filip S <filip.syn@protonmail.com>
2023-04-07 11:38:34 +0200

    Created interface for user service, added passing implementation

--- Zahradneek.Api/Services/UserService/IUserService.cs
+++ Zahradneek.Api/Services/UserService/IUserService.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Zahradneek.Api.Contracts.v1;
+
 namespace Zahradneek.Api.Services.UserService;
 
 public interface IUserService
 {
-    
+    public Task<UserInfoResponse> GetByIdAsync(Guid userId);
+    public Task<IEnumerable<UserInfoResponse>> GetAllAsync(Guid userId);
+    public Task<bool> CreateAsync(CreateUserRequest request);
+    public Task<bool> UpdateByIdAsync(UpdateUserRequest request, Guid userId);
+    public Task<bool> DeleteByIdAsync(Guid userId);
 }
--- Zahradneek.Api/Services/UserService/UserService.cs
+++ Zahradneek.Api/Services/UserService/UserService.cs
@@ -1,5 +1,31 @@
+using Zahradneek.Api.Contracts.v1;
+
 namespace Zahradneek.Api.Services.UserService;
 
 public class UserService : IUserService
 {
+    public async Task<UserInfoResponse> GetByIdAsync(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<UserInfoResponse>> GetAllAsync(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> CreateAsync(CreateUserRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> UpdateByIdAsync(UpdateUserRequest request, Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> DeleteByIdAsync(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
 }
