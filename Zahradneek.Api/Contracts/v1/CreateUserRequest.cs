fc73de28a5cfe39c91ec088a8d6179b58c663f14
Filip S <filip.syn@protonmail.com>
2023-04-07 11:39:01 +0200

    Added create and update request contracts for user service

---
+++ Zahradneek.Api/Contracts/v1/CreateUserRequest.cs
@@ -0,0 +1,12 @@
+namespace Zahradneek.Api.Contracts.v1;
+
+public record CreateUserRequest
+{
+    public string FirstName { get; init; } = string.Empty;
+    public string LastName { get; init; } = string.Empty;
+    public string Username { get; init; } = string.Empty;
+    public string Password { get; init; } = string.Empty;
+    public string Email { get; init; } = string.Empty;
+    public string PhoneNumber { get; init; } = string.Empty;
+    public DateOnly DateOfBirth { get; init; }
+}
