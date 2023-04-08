dc60919c61f626df7aafe1a65238691412a8da28
Filip S <filip.syn@protonmail.com>
2023-04-08 18:43:49 +0200

    Added validation to `CreateUserRequest`

--- Zahradneek.Api/Contracts/v1/CreateUserRequest.cs
+++ Zahradneek.Api/Contracts/v1/CreateUserRequest.cs
@@ -1,12 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Zahradneek.Api.Contracts.v1;
 
 public record CreateUserRequest
 {
-    public string FirstName { get; init; } = string.Empty;
-    public string LastName { get; init; } = string.Empty;
-    public string Username { get; init; } = string.Empty;
-    public string Password { get; init; } = string.Empty;
-    public string Email { get; init; } = string.Empty;
-    public string PhoneNumber { get; init; } = string.Empty;
-    public DateTime DateOfBirth { get; init; }
+    [Required] [StringLength(50)] public string FirstName { get; init; } = string.Empty;
+
+    [Required] [StringLength(50)] public string LastName { get; init; } = string.Empty;
+
+    [Required] [StringLength(50)] public string Username { get; init; } = string.Empty;
+    
+    [Required] public string Password { get; init; } = string.Empty;
+
+    [Required] [EmailAddress] public string Email { get; init; } = string.Empty;
+
+    [Required] [Phone] public string PhoneNumber { get; init; } = string.Empty;
+
+    [Required] public DateTime DateOfBirth { get; init; }
 }
