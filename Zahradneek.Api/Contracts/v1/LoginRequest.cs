0df5d7e824c4022fa3126b0a559223e3fbe1aa49
Filip S <filip.syn@protonmail.com>
2023-04-09 19:11:41 +0200

    Added default values for field in login request contract

--- Zahradneek.Api/Contracts/v1/LoginRequest.cs
+++ Zahradneek.Api/Contracts/v1/LoginRequest.cs
@@ -2,6 +2,6 @@ namespace Zahradneek.Api.Contracts.v1;
 
 public record LoginRequest
 {
-    public string Username { get; init; }
-    public string Password { get; init; }
+    public string Username { get; init; } = string.Empty;
+    public string Password { get; init; } = string.Empty;
 }
