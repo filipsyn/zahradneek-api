44223d065f4d8ba81731e404e64fc6e50075cc56
Filip S <filip.syn@protonmail.com>
2023-04-09 19:15:16 +0200

    Added validation to login request contract

--- Zahradneek.Api/Contracts/v1/LoginRequest.cs
+++ Zahradneek.Api/Contracts/v1/LoginRequest.cs
@@ -1,7 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Zahradneek.Api.Contracts.v1;
 
 public record LoginRequest
 {
-    public string Username { get; init; } = string.Empty;
-    public string Password { get; init; } = string.Empty;
+    [Required] public string Username { get; init; } = string.Empty;
+    [Required] public string Password { get; init; } = string.Empty;
 }
