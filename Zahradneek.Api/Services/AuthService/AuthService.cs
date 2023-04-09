608e2df9ea2844db606fe3959b1a42ce18faf92b
Filip S <filip.syn@protonmail.com>
2023-04-09 18:49:15 +0200

    Implemented helper methods for verifying login and generating JWT tokens

--- Zahradneek.Api/Services/AuthService/AuthService.cs
+++ Zahradneek.Api/Services/AuthService/AuthService.cs
@@ -1,5 +1,10 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
 using Zahradneek.Api.Contracts.v1;
 using Zahradneek.Api.Exceptions;
+using Zahradneek.Api.Models;
 using Zahradneek.Api.Repositories.UserRepository;
 using Zahradneek.Api.Services.UserService;
 
@@ -9,11 +14,13 @@ public class AuthService : IAuthService
 {
     private readonly IUserRepository _userRepository;
     private readonly IUserService _userService;
+    private readonly IConfiguration _configuration;
 
-    public AuthService(IUserRepository userRepository, IUserService userService)
+    public AuthService(IUserRepository userRepository, IUserService userService, IConfiguration configuration)
     {
         _userRepository = userRepository;
         _userService = userService;
+        _configuration = configuration;
     }
 
     public async Task RegisterAsync(CreateUserRequest request)
@@ -28,5 +35,37 @@ public class AuthService : IAuthService
     public async Task<string> LoginAsync(LoginRequest request)
     {
         throw new NotImplementedException();
+    }
+
+    private async Task<bool> VerifyPasswordAsync(LoginRequest request)
+    {
+        var user = await _userRepository.GetByUsernameAsync(request.Username);
+        if (user is null)
+            throw new NotFoundException($"User {request.Username} was not found");
+
+        return BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash);
+    }
+
+    private string GenerateJwtToken(User user)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Name, user.Username)
+        };
+
+        var key = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value!)
+        );
+
+        var credentials = new SigningCredentials(key: key, SecurityAlgorithms.HmacSha512Signature);
+
+        var token = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.Now.AddHours(1),
+            signingCredentials: credentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
     }
 }
