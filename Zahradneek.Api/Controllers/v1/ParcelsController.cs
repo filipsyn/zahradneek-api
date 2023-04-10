705f0812f4ff4dd3f3c7ca7a1ce3a6dc331dd2fa
Filip S <filip.syn@protonmail.com>
2023-04-10 16:31:01 +0200

    Created parcels controller

---
+++ Zahradneek.Api/Controllers/v1/ParcelsController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Zahradneek.Api.Controllers.v1;
+
+[ApiController]
+[Route("/v1/parcels")]
+[Produces("application/json")]
+public class ParcelsController : ControllerBase
+{
+    
+}
