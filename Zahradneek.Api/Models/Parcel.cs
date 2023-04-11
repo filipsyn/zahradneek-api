769e62e8054bc609a08044b76367bf6ed90948ac
Filip S <filip.syn@protonmail.com>
2023-04-11 13:46:53 +0200

    Adjusted parcel model
    Removed creating of new instance of Owner property.
    Added Json Ignore attribute to Owner property.
    Removed creating of new instance of Coordinates property.

--- Zahradneek.Api/Models/Parcel.cs
+++ Zahradneek.Api/Models/Parcel.cs
@@ -1,15 +1,18 @@
+using Newtonsoft.Json;
+
 namespace Zahradneek.Api.Models;
 
 public class Parcel : BaseEntity
 {
     public string? Name { get; set; }
-    
+
     // Relationships
-    
+
     // Relationship with User
     public int OwnerId { get; set; }
-    public User Owner { get; set; } = new User();
-    
+    [JsonIgnore]
+    public User Owner { get; set; }
+
     // Relationship with Coordinates
-    public List<Coordinate> Coordinates { get; set; } = new List<Coordinate>();
+    public List<Coordinate> Coordinates { get; set; }
 }
