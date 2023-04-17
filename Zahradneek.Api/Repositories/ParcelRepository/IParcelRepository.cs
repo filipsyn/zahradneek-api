6cb8733f37b0b8a244f6b61388abcf5400b2ba24
Filip S <filip.syn@protonmail.com>
2023-04-17 14:57:10 +0200

    Added parcel repository interface

--- Zahradneek.Api/Repositories/ParcelRepository/IParcelRepository.cs
+++ Zahradneek.Api/Repositories/ParcelRepository/IParcelRepository.cs
@@ -4,4 +4,15 @@ namespace Zahradneek.Api.Repositories.ParcelRepository;
 
 public interface IParcelRepository
 {
+    public Task<IEnumerable<Parcel>> GetAllAsync();
+
+    public Task<Parcel?> GetById(int parcelId);
+
+    public Task<IEnumerable<Parcel>> GetAllByOwnerId(int ownerId);
+
+    public Task CreateAsync(Parcel parcel);
+
+    public Task UpdateByIdAsync(Parcel parcel, int parcelId);
+
+    public Task DeleteByIdAsync(int parcelId);
 }
--- Zahradneek.Api/Repositories/ParcelRepository/ParcelRepository.cs
+++ Zahradneek.Api/Repositories/ParcelRepository/ParcelRepository.cs
@@ -1,6 +1,36 @@
+using Zahradneek.Api.Models;
+
 namespace Zahradneek.Api.Repositories.ParcelRepository;
 
 public class ParcelRepository : IParcelRepository
 {
-    
+    public async Task<IEnumerable<Parcel>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Parcel?> GetById(int parcelId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<Parcel>> GetAllByOwnerId(int ownerId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task CreateAsync(Parcel parcel)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task UpdateByIdAsync(Parcel parcel, int parcelId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task DeleteByIdAsync(int parcelId)
+    {
+        throw new NotImplementedException();
+    }
 }
