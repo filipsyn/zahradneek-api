cd5407a885ededce5f37868e0e415efe5c29c59f
Filip S <filip.syn@protonmail.com>
2023-04-23 16:15:14 +0200

    Added parcels controller

--- Zahradneek.Api/Controllers/v1/ParcelsController.cs
+++ Zahradneek.Api/Controllers/v1/ParcelsController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Zahradneek.Api.Contracts.v1;
+using Zahradneek.Api.Services.ParcelService;
 
 namespace Zahradneek.Api.Controllers.v1;
 
@@ -7,5 +9,47 @@ namespace Zahradneek.Api.Controllers.v1;
 [Produces("application/json")]
 public class ParcelsController : ControllerBase
 {
-    
+    private readonly IParcelService _parcelService;
+
+    public ParcelsController(IParcelService parcelService)
+    {
+        _parcelService = parcelService;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(type: typeof(IEnumerable<ParcelInfoResponse>), statusCode: StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAll() =>
+        Ok(await _parcelService.GetAllAsync());
+
+    [HttpGet("{parcelId:int}")]
+    [ProducesResponseType(type: typeof(ParcelInfoResponse), statusCode: StatusCodes.Status200OK)]
+    [ProducesResponseType(statusCode: StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetById([FromRoute] int parcelId) =>
+        Ok(await _parcelService.GetByIdAsync(parcelId));
+
+    [HttpPost]
+    [ProducesResponseType(statusCode: StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> Create([FromBody] CreateParcelRequest request)
+    {
+        await _parcelService.CreateAsync(request);
+        return NoContent();
+    }
+
+    [HttpPut("{parcelId:int}")]
+    [ProducesResponseType(statusCode: StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> UpdateById([FromBody] UpdateParcelRequest request, [FromRoute] int parcelId)
+    {
+        await _parcelService.UpdateByIdAsync(request: request, parcelId: parcelId);
+        return NoContent();
+    }
+
+    [HttpDelete("{parcelId:int}")]
+    [ProducesResponseType(statusCode: StatusCodes.Status204NoContent)]
+    [ProducesResponseType(statusCode: StatusCodes.Status404NotFound)]
+    [ProducesResponseType(statusCode: StatusCodes.Status409Conflict)]
+    public async Task<IActionResult> DeleteById([FromRoute] int parcelId)
+    {
+        await _parcelService.DeleteByIdAsync(parcelId);
+        return NoContent();
+    }
 }
