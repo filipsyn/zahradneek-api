c3aa9d5fdc96a1711a807e3d91a0a3f6142cc843
Filip S <filip.syn@protonmail.com>
2023-05-07 13:47:56 +0200

    Add interface members for `ICoordinateService`

--- Zahradneek.Api/Services/CoordinateService/ICoordinateService.cs
+++ Zahradneek.Api/Services/CoordinateService/ICoordinateService.cs
@@ -1,6 +1,24 @@
+using Zahradneek.Api.Contracts.v1;
+
 namespace Zahradneek.Api.Services.CoordinateService;
 
 public interface ICoordinateService
 {
-    
+    // Get all
+    public Task<IEnumerable<CoordinateInfoResponse>> GetAllAsync();
+
+    // Get all for parcel
+    public Task<IEnumerable<CoordinateInfoResponse>> GetAllForParcel(int parcelId);
+
+    // Get by id
+    public Task<CoordinateInfoResponse> GetByIdAsync(int coordinateId);
+
+    // Create async
+    public Task CreateAsync(CreateCoordinateRequest request);
+
+    // Update by id
+    public Task UpdateByIdAsync(int coordinateId, UpdateCoordinateRequest request);
+
+    // Delete by id
+    public Task DeleteByIdAsync(int coordinateId);
 }
