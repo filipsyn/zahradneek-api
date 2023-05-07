0e43d1994db18868ad62f3b0c22b92803f9accb0
Filip Synek <xjust@mendelu.cz>
2023-05-07 11:58:52 +0200

    Inject `DataContext` and `AutoMapper` into `CoordinateRepository`

--- Zahradneek.Api/Repositories/CoordinateRepository/CoordinateRepository.cs
+++ Zahradneek.Api/Repositories/CoordinateRepository/CoordinateRepository.cs
@@ -1,12 +1,20 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Zahradneek.Api.Data;
+using Zahradneek.Api.Exceptions;
 using Zahradneek.Api.Models;
 
 namespace Zahradneek.Api.Repositories.CoordinateRepository;
 
 public class CoordinateRepository : ICoordinateRepository
 {
-    public async Task<IEnumerable<Coordinate>> GetAllAsync()
+    private readonly DataContext _db;
+    private readonly IMapper _mapper;
+
+    public CoordinateRepository(DataContext db, IMapper mapper)
     {
-        throw new NotImplementedException();
+        _db = db;
+        _mapper = mapper;
     }
 
     public async Task<IEnumerable<Coordinate>> GetAllForParcelAsync(int parcelId)
