7fb91117cebe71e26067f85e054eb720482573e8
Filip S <filip.syn@protonmail.com>
2023-05-08 16:22:49 +0200

    Change type of `Coordinates` filed to be list of `UpdateCoordinateRequest`

--- Zahradneek.Api/Contracts/v1/Requests/UpdateParcelRequest.cs
+++ Zahradneek.Api/Contracts/v1/Requests/UpdateParcelRequest.cs
@@ -5,5 +5,5 @@ namespace Zahradneek.Api.Contracts.v1.Requests;
 public record UpdateParcelRequest
 {
     public string? Name { get; init; }
-    public List<Coordinate>? Coordinates { get; init; }
+    public List<UpdateCoordinateRequest>? Coordinates { get; init; }
 };
