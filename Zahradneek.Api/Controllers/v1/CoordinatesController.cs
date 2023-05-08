a486ff4e7358dbdc1ce32e3727d22b8d01e1db4c
Filip S <filip.syn@protonmail.com>
2023-05-08 15:51:34 +0200

    Implement `Create` endpoint in `CoordinatesController`

--- Zahradneek.Api/Controllers/v1/CoordinatesController.cs
+++ Zahradneek.Api/Controllers/v1/CoordinatesController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Zahradneek.Api.Contracts.v1.Requests;
 using Zahradneek.Api.Contracts.v1.Responses;
 using Zahradneek.Api.Services.CoordinateService;
 
@@ -24,4 +25,12 @@ public class CoordinatesController : ControllerBase
     [HttpGet("{coordinateId:int}")]
     public async Task<IActionResult> GetById([FromRoute] int coordinateId) =>
         Ok(await _coordinateService.GetByIdAsync(coordinateId));
+
+    [HttpPost]
+    [ProducesResponseType(statusCode: StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> Create([FromBody] CreateCoordinateRequest request)
+    {
+        await _coordinateService.CreateAsync(request);
+        return NoContent();
+    }
 }
