4f66b10b947f2e52e561cd914fc9995a828187d4
Filip S <filip.syn@protonmail.com>
2023-05-08 15:45:54 +0200

    Add `Required` annotations to create and update coordinate request contracts

--- Zahradneek.Api/Contracts/v1/Requests/CreateCoordinateRequest.cs
+++ Zahradneek.Api/Contracts/v1/Requests/CreateCoordinateRequest.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Zahradneek.Api.Contracts.v1.Requests;
 
 public record CreateCoordinateRequest
 {
-    public double Longitude { get; set; }
-    public double Latitude { get; set; }
+    [Required] public double Longitude { get; set; }
+    [Required] public double Latitude { get; set; }
 
-    public int ParcelId { get; set; }
+    [Required] public int ParcelId { get; set; }
 }
--- Zahradneek.Api/Contracts/v1/Requests/UpdateCoordinateRequest.cs
+++ Zahradneek.Api/Contracts/v1/Requests/UpdateCoordinateRequest.cs
@@ -1,7 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Zahradneek.Api.Contracts.v1.Requests;
 
 public record UpdateCoordinateRequest
 {
-    public double Longitude { get; set; }
-    public double Latitude { get; set; }
+    [Required] public double Longitude { get; set; }
+    [Required] public double Latitude { get; set; }
 }
