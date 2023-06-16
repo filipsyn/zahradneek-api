9c6b50b33b46790047937c8ccf64f2f8770d5e58
Filip S <filip.syn@protonmail.com>
2023-06-16 16:33:38 +0200

    Apply migration that adds `water_logs` table

---
+++ Zahradneek.Api/Migrations/20230616142950_Add_WaterLogs_Table.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Zahradneek.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_WaterLogs_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "water_logs",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    amount = table.Column<float>(type: "float", nullable: false),
+                    parcel_id = table.Column<int>(type: "int", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    modified_at = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_water_logs", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_water_logs_parcels_parcel_id",
+                        column: x => x.parcel_id,
+                        principalTable: "parcels",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_water_logs_parcel_id",
+                table: "water_logs",
+                column: "parcel_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "water_logs");
+        }
+    }
+}
