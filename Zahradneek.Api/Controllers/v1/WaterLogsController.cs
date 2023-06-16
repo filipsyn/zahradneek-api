4482edd32b908a051e5c9b0419777217068b1300
Filip S <filip.syn@protonmail.com>
2023-06-16 17:41:13 +0200

    Add `GetAll` endpoint to `WaterLogsController`

---
+++ Zahradneek.Api/Controllers/v1/WaterLogsController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Zahradneek.Api.Contracts.v1.Responses;
+using Zahradneek.Api.Services.WaterLogService;
+
+namespace Zahradneek.Api.Controllers.v1;
+
+[ApiController]
+[Route("v1/water-logs")]
+[Produces("application/json")]
+public class WaterLogsController : ControllerBase
+{
+    private readonly IWaterLogService _waterLogService;
+
+    public WaterLogsController(IWaterLogService waterLogService)
+    {
+        _waterLogService = waterLogService;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(statusCode: StatusCodes.Status200OK, type: typeof(IEnumerable<WaterLogInfoResponse>))]
+    public async Task<IActionResult> GetAll() => Ok(await _waterLogService.GetAllAsync());
+}
