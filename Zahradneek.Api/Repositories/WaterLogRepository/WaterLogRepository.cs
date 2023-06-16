9f0e24f52d05dce5be3157cb1e4b04943f2e7cf4
Filip S <filip.syn@protonmail.com>
2023-06-16 17:01:28 +0200

    Add implementation to `WaterLogRepository`

---
+++ Zahradneek.Api/Repositories/WaterLogRepository/WaterLogRepository.cs
@@ -0,0 +1,60 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Zahradneek.Api.Data;
+using Zahradneek.Api.Exceptions;
+using Zahradneek.Api.Models;
+
+namespace Zahradneek.Api.Repositories.WaterLogRepository;
+
+public class WaterLogRepository : IWaterLogRepository
+{
+    private readonly DataContext _db;
+    private readonly IMapper _mapper;
+
+    public WaterLogRepository(DataContext db, IMapper mapper)
+    {
+        _db = db;
+        _mapper = mapper;
+    }
+
+    public async Task<WaterLog?> GetByIdAsync(int waterLogId)
+        => await _db.WaterLogs.FirstOrDefaultAsync(log => log.Id == waterLogId);
+
+    public async Task<IEnumerable<WaterLog>> GetAllAsync()
+        => await _db.WaterLogs.ToListAsync();
+
+    public async Task<IEnumerable<WaterLog>> GetAllForParcelIdAsync(int parcelId)
+        => await _db.WaterLogs
+            .Include(x => x.Parcel)
+            .Where(x => x.ParcelId == parcelId)
+            .ToListAsync();
+
+    public async Task CreateAsync(WaterLog waterLog)
+    {
+        _db.WaterLogs.Add(waterLog);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(WaterLog updatedWaterLog, int waterLogId)
+    {
+        var waterLog = await GetByIdAsync(waterLogId);
+        if (waterLog is null)
+            throw new NotFoundException("Water Log was not found");
+
+        _mapper.Map(updatedWaterLog, waterLog);
+
+        _db.WaterLogs.Update(waterLog);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task DeleteByIdAsync(int waterLogId)
+    {
+        var foundWaterLog = await GetByIdAsync(waterLogId);
+
+        if (foundWaterLog is null)
+            throw new NotFoundException("Water Log was not found");
+
+        _db.WaterLogs.Remove(foundWaterLog);
+        await _db.SaveChangesAsync();
+    }
+}
