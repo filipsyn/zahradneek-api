e7cbb33af5f11be11de8e62c5341f752f9c1d690
Filip S <filip.syn@protonmail.com>
2023-06-17 18:11:23 +0200

    Apply migrations with `role` attribute added to `users table`

---
+++ Zahradneek.Api/Migrations/20230617160802_Add_Role_Field_To_User.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Zahradneek.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Role_Field_To_User : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "role",
+                table: "users",
+                type: "longtext",
+                nullable: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "role",
+                table: "users");
+        }
+    }
+}
