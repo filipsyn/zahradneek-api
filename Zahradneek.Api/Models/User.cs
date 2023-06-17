ff9049535387166a45c45b535220eb4b9038bec6
Filip S <filip.syn@protonmail.com>
2023-06-17 18:10:36 +0200

    Add `Role` filed to `User` model

--- Zahradneek.Api/Models/User.cs
+++ Zahradneek.Api/Models/User.cs
@@ -6,11 +6,11 @@ public class User : BaseEntity
     public string LastName { get; set; } = string.Empty;
     public string Username { get; set; } = string.Empty;
     public string PasswordHash { get; set; } = string.Empty;
-
+    public string Role { get; set; } = "User";
     public string Email { get; set; } = string.Empty;
     public string PhoneNumber { get; set; } = string.Empty;
     public DateTime DateOfBirth { get; set; }
-    
+
     // Relationships
     public List<Parcel> Parcels { get; set; }
 }
