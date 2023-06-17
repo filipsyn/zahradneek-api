e706af0d11ad94236d513db2aa996f2d4a4794ea
Filip S <filip.syn@protonmail.com>
2023-06-17 12:45:03 +0200

    Add members to `INewsService`

--- Zahradneek.Api/Services/NewsService/INewsService.cs
+++ Zahradneek.Api/Services/NewsService/INewsService.cs
@@ -1,6 +1,16 @@
+using Zahradneek.Api.Contracts.v1.Requests;
+using Zahradneek.Api.Contracts.v1.Responses;
+
 namespace Zahradneek.Api.Services.NewsService;
 
 public interface INewsService
 {
-    
+    public Task<IEnumerable<NewsInfoResponse>> GetAllAsync();
+    public Task<NewsInfoResponse> GetByIdAsync(int newsId);
+
+    public Task CreateAsync(CreateNewsRequest request);
+
+    public Task UpdateByIdAsync(UpdateNewsRequest request, int newsId);
+
+    public Task DeleteByIdAsync(int newsId);
 }
