3895298cde27c7ceaae15b3b1b151125a168e20c
Filip S <filip.syn@protonmail.com>
2023-06-17 11:39:40 +0200

    Fix parcel creation request
    Request to create parcel wouldn't go through because the request would not provide all of necessary info about parcel

--- Zahradneek.Api/Contracts/v1/Requests/CreateParcelRequest.cs
+++ Zahradneek.Api/Contracts/v1/Requests/CreateParcelRequest.cs
@@ -1,9 +1,9 @@
-using Zahradneek.Api.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Zahradneek.Api.Contracts.v1.Requests;
 
 public record CreateParcelRequest
 {
     public string? Name { get; init; }
-    public List<Coordinate>? Coordinates { get; init; }
+    [Required] public int OwnerId { get; set; }
 };
