4c06482ee1addad8d67a7422b49f08a3d6b85f0c
Filip S <filip.syn@protonmail.com>
2023-06-17 12:15:04 +0200

    Add members to `INewsReposiotry`

--- Zahradneek.Api/Repositories/NewsRepository/INewsRepository.cs
+++ Zahradneek.Api/Repositories/NewsRepository/INewsRepository.cs
@@ -1,6 +1,16 @@
+using Zahradneek.Api.Models;
+
 namespace Zahradneek.Api.Repositories.NewsRepository;
 
 public interface INewsRepository
 {
-    
+    public Task<IEnumerable<News>> GetAllAsync();
+
+    public Task<News?> GetById(int newsId);
+
+    public Task CreateAsync(News news);
+
+    public Task UpdateByIdAsync(News news, int newsId);
+
+    public Task DeleteByIdAsync(int newsId);
 }
