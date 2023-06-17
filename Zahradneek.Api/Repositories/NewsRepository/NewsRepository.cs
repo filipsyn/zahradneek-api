a91dca36a076717f77029b59645db4cf58c4278b
Filip S <filip.syn@protonmail.com>
2023-06-17 12:25:32 +0200

    Implement interface members in `NewsRepository`

--- Zahradneek.Api/Repositories/NewsRepository/NewsRepository.cs
+++ Zahradneek.Api/Repositories/NewsRepository/NewsRepository.cs
@@ -1,6 +1,56 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Zahradneek.Api.Data;
+using Zahradneek.Api.Exceptions;
+using Zahradneek.Api.Models;
+
 namespace Zahradneek.Api.Repositories.NewsRepository;
 
-public class NewsRepository: INewsRepository
+public class NewsRepository : INewsRepository
 {
-    
+    private readonly DataContext _db;
+    private readonly IMapper _mapper;
+
+    public NewsRepository(DataContext db, IMapper mapper)
+    {
+        _db = db;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<News>> GetAllAsync()
+        => await _db.News.ToListAsync();
+
+    public async Task<News?> GetById(int newsId) =>
+        await _db.News
+            .Where(n => n.Id == newsId)
+            .FirstOrDefaultAsync();
+
+    public async Task CreateAsync(News news)
+    {
+        _db.News.Add(news);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task UpdateByIdAsync(News updatedNews, int newsId)
+    {
+        var news = await this.GetById(newsId);
+        if (news is null)
+            throw new NotFoundException("News article was not found");
+
+        _mapper.Map(updatedNews, news);
+        news.AuthorId = updatedNews.AuthorId;
+
+        _db.News.Update(news);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task DeleteByIdAsync(int newsId)
+    {
+        var foundNews = await this.GetById(newsId);
+        if (foundNews is null)
+            throw new NotFoundException("News article was not found");
+
+        _db.News.Remove(foundNews);
+        await _db.SaveChangesAsync();
+    }
 }
