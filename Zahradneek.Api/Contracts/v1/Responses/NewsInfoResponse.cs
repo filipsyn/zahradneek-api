2af7df2604ad1b3c0f58658670be1354c19fc819
Filip S <filip.syn@protonmail.com>
2023-06-19 10:38:10 +0200

    Add `Id`, `CreatedAt` and `ModifiedAt` fields to `NewsInfoResponse`

--- Zahradneek.Api/Contracts/v1/Responses/NewsInfoResponse.cs
+++ Zahradneek.Api/Contracts/v1/Responses/NewsInfoResponse.cs
@@ -2,7 +2,10 @@ namespace Zahradneek.Api.Contracts.v1.Responses;
 
 public record NewsInfoResponse
 {
+    public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Content { get; set; } = string.Empty;
     public int AuthorId { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime ModifiedAt { get; set; }
 }
